6f3438f84a7b6b324f040bcb2c1865b6a089a459
Alex Oancea <alx.oancea@gmail.com>
2016-09-24 21:35:57 +0300

    project, unit testing project, ci config

---
+++ src/AutoMapper.Extensions.UnitTesting/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoMapper.Extensions.UnitTesting
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
